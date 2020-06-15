881cf3f74aad1de1a2159b50ba79533b3b9a38ce
AaronShah2 <47093449+AaronShah2@users.noreply.github.com>
2020-06-14 23:22:01 -0700

    Added ability to search for most profittable majors in school.

--- CollegeMajor/settings.py
+++ CollegeMajor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iar66%=md5g+tc@vr(k(y3)5)hv214r6z&0f@qg^b03zey#d#q'
+SECRET_KEY = passwords.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- main/views.py
+++ main/views.py
@@ -8,17 +8,49 @@ from collections import defaultdict
 def index(request):
     # dictionary of key:value pairs sent to front end
     context = {}
-    universities = defaultdict(dict)
+
+    # display schools that pay the highest for a given major to front-end
     major = None
     if "major" in request.GET.keys():
         major = request.GET['major'].lower()
+        context['schools'] = highest_major(major)
 
+    # display top paying majors for a given university to front-end
+    school = None
+    if "school" in request.GET.keys():
+        major = request.GET['school'].lower()
+        context['majors'] = highest_school(major)
+
+    #returns output
+    return render(request, "index.html", context)
+
+# returns dictionary of universities that pay the highest for a given major
+def highest_major(major):
+    # universities that recieve highest pay from major
+    highest_universities = defaultdict(dict)
+
+    # if major is valid, then pulls top 10 highest paying schools
     if major is not None:
         query1 = f"SELECT * FROM collegemajor WHERE cipdesc = '{major}' " \
                  f"and credlev = '3' order by md_earn_wne::int desc LIMIT 10;"
         for m in Collegemajor.objects.raw(query1):
-            universities[m.instnm]["median_earnings"] = m.md_earn_wne
+            highest_universities[m.instnm]["median_earnings"] = m.md_earn_wne
 
-    context['universities'] = dict(universities)
-    print(universities)
-    return render(request, "index.html", context)
+    #returns dictionary
+    return dict(highest_universities)
+
+# returns dictionary of majors that pay the highest for a given school
+def highest_school(school):
+    # majors that recieve highest pay in school
+    highest_majors = defaultdict(dict)
+
+    # if major is valid, then pulls top 10 highest paying schools
+    if school is not None:
+        query1 = f"SELECT * FROM collegemajor WHERE instnm = '{school}' " \
+                 f"and credlev = '3' order by md_earn_wne::int desc LIMIT 10;"
+        for m in Collegemajor.objects.raw(query1):
+
+            highest_majors[m.cipdesc]["median_earnings"] = m.md_earn_wne
+
+    #returns dictionary
+        return dict(highest_majors)
