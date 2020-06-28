6524d0870c09758320028fde7bd8141936ad48b1
bncosta <bncosta@uci.edu>
2020-06-27 18:08:20 -0700

    cleaner code, added graph for average major salary

--- main/views.py
+++ main/views.py
@@ -1,19 +1,14 @@
-# from django.http import HttpResponse, HttpResponseRedirect
-import decimal
-
 from django.shortcuts import render
 from main.models import Collegemajor
 from collections import defaultdict
 from django.db import connection
 from django.views.generic import TemplateView
-from django.views.generic import FormView
 from main.forms import MajorForm, SchoolForm
 from dal import autocomplete
-import json
 
 """
 - Used for Index.html page
-- Allows users to search majors by price & universities
+- Allows users to search majors by earnings & universities
 """
 
 
@@ -36,9 +31,10 @@ class CollegeMajor(TemplateView):
         # display schools that pay the highest for a given major to front-end
         if "major" in request.GET.keys():
             major = request.GET['major'].lower()
-            context['schools'], context['average_salary'], \
-            context['uni_names_pub'], context['uni_names_priv_np'], context['uni_names_priv_fp'],\
-            context['coordinate_points_pub'], context['coordinate_points_priv_np'], context['coordinate_points_priv_fp'] = highest_major(major)
+            context['schools'], context['average_salary'], context['uni_names_pub'], context['uni_names_priv_np'], \
+            context['uni_names_priv_fp'], context['coordinate_points_pub'], context['coordinate_points_priv_np'], \
+            context['coordinate_points_priv_fp'] = highest_major(major)
+
             context['major'] = major.title()
 
         # display top paying majors for a given university to front-end
@@ -47,20 +43,17 @@ class CollegeMajor(TemplateView):
             context['majors'], context["major_names"], context["coordinate_points"] = highest_school(school)
 
         if "highest_avg" in request.GET.keys():
-            context['majors'] = highest_avg_major()
+            context['majors'], context["major_names"], context["coordinate_points"] = highest_avg_major()
 
         # returns output
         return render(request, "index.html", context)
 
-    # TESTING PHASE interprets form data
-    def post(self, request):
-        majors = self.mForm(request.POST)
-        if majors.is_valid():
-            print('yay, it works!')
-        else:
-            print('Rats!')
-            print(majors.cleaned_data['cipdesc'])
-        return render(request, "index.html")
+    # # TESTING PHASE interprets form data
+    # def post(self, request):
+    #     majors = self.mForm(request.POST)
+    #     if not majors.is_valid():
+    #         print(majors.cleaned_data['cipdesc'])
+    #     return render(request, "index.html")
 
 
 class MajorAutoComplete(autocomplete.Select2QuerySetView):
@@ -100,7 +93,7 @@ class SchoolAutoComplete(autocomplete.Select2QuerySetView):
 
         return schools
 
-    # Because we are only unpacking a litst of schools, we simply return the string of the tuple instead of pk obj
+    # Because we are only unpacking a list of schools, we simply return the string of the tuple instead of pk obj
     # (pk objects help organize serialized data)
     def get_result_value(self, result):
         return result
@@ -118,47 +111,56 @@ Helper Methods - (Could possibly be reimplemented in class?)
 
 # returns dictionary of universities that pay the highest for a given major
 def highest_major(major):
+    print(major)
+    if major is None or "":
+        return None
+
     # universities that receive highest pay from major
     highest_universities = defaultdict(dict)
 
-    # if major is valid, then pulls top 10 highest paying schools
-    if major is not None:
-        major = major.replace("'", "''")
-        major = major.replace('"', '""')
-        query1 = f"SELECT * FROM collegemajor WHERE cipdesc = '{major}' " \
-                 f"and credlev = '3' order by md_earn_wne::int desc;"
-        uni_names ={"Private, nonprofit":[],
-                     "Public":[],
-                     "Private, for-profit":[]} # names of universities, catagorized by private / public
-        coordinate_points = {"Private, nonprofit":[],
-                     "Public":[],
-                     "Private, for-profit":[]} #list of data points that will be passed to front end
-        for m in Collegemajor.objects.raw(query1):
-            highest_universities[m.instnm]["median_earnings"] = m.md_earn_wne
-            coords = {'x':m.md_earn_wne,'y':m.debtmedian}
-            if coords['y'] != 'PrivacySuppressed':
-                if m.control == "Private, nonprofit":
-                    coordinate_points["Private, nonprofit"].append(coords)
-                    uni_names["Private, nonprofit"].append(m.instnm.title())
-                elif m.control == "Public":
-                    coordinate_points["Public"].append(coords)
-                    uni_names["Public"].append(m.instnm.title())
-                elif m.control == "Private, for-profit":
-                    coordinate_points["Private, for-profit"].append(coords)
-                    uni_names["Private, for-profit"].append(m.instnm.title())
-        avg_salary = None
+    major = major.replace("'", "''")
+    major = major.replace('"', '""')
+    query1 = f"SELECT * FROM collegemajor WHERE cipdesc = '{major}' " \
+             f"and credlev = '3' order by md_earn_wne::int desc;"
 
-        # make sure results were returned for this major
-        if len(highest_universities) != 0:
-            with connection.cursor() as cursor:
-                query2 = f"SELECT AVG(NULLIF(md_earn_wne, '')::int)  FROM collegemajor " \
-                         f"WHERE cipdesc = '{major}' and credlev = '3';"
-                cursor.execute(query2)
-                avg_salary = round(cursor.fetchone()[0], -2)
+    # names of universities, categorized by private / public
+    uni_names = {"Private, nonprofit": [], "Public": [], "Private, for-profit": []}
+
+    # list of data points that will be passed to front end
+    coordinate_points = {"Private, nonprofit": [], "Public": [], "Private, for-profit": []}
+
+    for m in Collegemajor.objects.raw(query1):
+        highest_universities[m.instnm]["median_earnings"] = m.md_earn_wne
+        coords = {'x': m.md_earn_wne, 'y': m.debtmedian}
+
+        if coords['y'] != 'PrivacySuppressed':
+            if m.control == "Private, nonprofit":
+                coordinate_points["Private, nonprofit"].append(coords)
+                uni_names["Private, nonprofit"].append(m.instnm.title())
+
+            elif m.control == "Public":
+                coordinate_points["Public"].append(coords)
+                uni_names["Public"].append(m.instnm.title())
+
+            elif m.control == "Private, for-profit":
+                coordinate_points["Private, for-profit"].append(coords)
+                uni_names["Private, for-profit"].append(m.instnm.title())
+
+    avg_salary = None
+
+    # first makes sure there are results for this major (user didnt input non existent major)
+    # next, gets the average pay for this major across all universities
+    if len(highest_universities) != 0:
+        with connection.cursor() as cursor:
+            query2 = f"SELECT AVG(NULLIF(md_earn_wne, '')::int)  FROM collegemajor WHERE cipdesc = '{major}' " \
+                     f"and credlev = '3';"
+            cursor.execute(query2)
+            avg_salary = int(round(cursor.fetchone()[0], -2))
+
     # returns dictionary
-    return dict(highest_universities), avg_salary, \
-           uni_names["Public"], uni_names["Private, nonprofit"], uni_names["Private, for-profit"],\
-           coordinate_points["Public"], coordinate_points["Private, nonprofit"], coordinate_points["Private, for-profit"]
+    return dict(highest_universities), avg_salary, uni_names["Public"], uni_names["Private, nonprofit"], \
+           uni_names["Private, for-profit"], coordinate_points["Public"], coordinate_points["Private, nonprofit"], \
+           coordinate_points["Private, for-profit"]
 
 
 # returns dictionary of majors that pay the highest for a given school
@@ -168,15 +170,22 @@ def highest_school(school):
 
     # if major is valid, then pulls top 10 highest paying schools
     if school is not None:
+        # if the school name has a single or double quote (ie: Saint Mary's),
+        # replace it with two single or double quotes. This is equivalent to escape sequencing for PostGreSql
         school = school.replace("'", "''")
         school = school.replace('"', '""')
-        query1 = f"SELECT * FROM collegemajor WHERE instnm = '{school}' " \
+        query1 = f"SELECT id, cipdesc, md_earn_wne, debtmedian FROM collegemajor WHERE instnm = '{school}' " \
                  f"and credlev = '3' order by md_earn_wne::int desc;"
-        major_names = [] # names of majors that the university teaches
-        coordinate_points = [] # data points that will be passed to front-end
+
+        # names of majors that the university teaches
+        major_names = []
+
+        # data points that will be passed to front-end
+        coordinate_points = []
 
         for m in Collegemajor.objects.raw(query1):
             highest_majors[m.cipdesc]["median_earnings"] = m.md_earn_wne
+            highest_majors[m.cipdesc]["median_debt"] = m.debtmedian
             coords = {'x': m.md_earn_wne, 'y': m.debtmedian}
             major_names.append(m.cipdesc.title())
             coordinate_points.append(coords)
@@ -185,20 +194,32 @@ def highest_school(school):
         return dict(highest_majors), major_names, coordinate_points
 
 
-# returns dictionary of majors by average starting salary
+# returns dictionary of majors by average starting salary across all universities
 def highest_avg_major():
     # majors that receive highest pay in school
     highest_majors = defaultdict(dict)
 
     with connection.cursor() as cursor:
         # if major is valid, then pulls top 10 highest paying schools
-        query1 = f"SELECT AVG(NULLIF(md_earn_wne, '')::int) as average, cipdesc  FROM collegemajor " \
-                 f"WHERE credlev = '3' group by cipdesc order by average desc;"
+        query1 = f"SELECT AVG(NULLIF(md_earn_wne, '')::int) as average_earnings, " \
+                 f"AVG(NULLIF(debtmedian, 'PrivacySuppressed')::int) as average_debt, cipdesc  FROM collegemajor " \
+                 f"WHERE credlev = '3' group by cipdesc order by average_earnings desc;"
 
         cursor.execute(query1)
-        for avg_salary, major_name in cursor.fetchall():
-            avg_salary = round(avg_salary, -2)
-            highest_majors[major_name]["median_earnings"] = avg_salary
+        major_names = list()
+        coordinate_points = list()
+        for avg_salary, median_debt, major_name in cursor.fetchall():
+            # note average_debt may be none if there is debt data available for any of the schools that had the major
+            if median_debt is not None:
+
+                # averages will be decimals, too specific and not accurate to be a float given significant figures
+                avg_salary = int(round(avg_salary, -2))
+                median_debt = int(round(median_debt, -2))
+                highest_majors[major_name]["median_earnings"] = avg_salary
+                highest_majors[major_name]["median_debt"] = median_debt
+                coords = {'x': avg_salary, 'y': median_debt}
+                major_names.append(major_name.title())
+                coordinate_points.append(coords)
 
         # returns dictionary
-        return dict(highest_majors)
+        return dict(highest_majors), major_names, coordinate_points
